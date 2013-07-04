8a1d9d358522019a1f991fdf21a7d1a99390df0e
vitrum.zhu <vitrum.cn@gmail.com>
2013-07-04 18:45:27 +0800

    update style

--- js/common.js
+++ js/common.js
@@ -1,3 +1,160 @@
+
+function showFrame(framename) {
+  if(!framename){ framename = 'homepage'}
+  $('.frame').hide();
+  if(framename !=='homepage' ){ $('.link_home').show();} else {$('.link_home').show();};
+  $('.' + framename ).show();
+  setTimeout(function(){window.scrollTo(0, 0);}, 0);
+}
+
+function showSubFrame(framename,subframename) {
+  if(!framename && !subframename) {return false;};
+  showFrame(framename);
+  $('.' + framename + ' .subframe').hide();
+  $('.' + framename + ' .' + subframename).show();
+}
+
+function showNavBar(barname) {
+  if(!barname){ barname = 'aboutgame'}
+  $('.navbar').hide();
+  $('.navbox').show();
+  $('.' + barname ).show();
+  //check nav bar prostion;
+}
+
+function gotPic(event) {
+  if(event.target.files.length == 1 && 
+    event.target.files[0].type.indexOf("image/") == 0) {
+    $("#yourimage").attr("src",URL.createObjectURL(event.target.files[0]));
+    showNavBar('retakepic');
+  }
+}
+
+function postThePic(event) {
+  //alert("postThePic OK!");
+  router.navigate('yourmount/take');
+  console.log('postThePic OK!');
+}
+
+function drowMout(mountid) {
+  var scale = 2
+  , stage = new Kinetic.Stage({
+    container: 'container',
+    width: 300,
+    height: 280
+  });
+  var stage = new Kinetic.Stage({
+    container: 'container',
+    width: 300,
+    height: 280
+  });
+  var staticLayer = new Kinetic.Layer();
+  var staticGroup = new Kinetic.Group({
+    x: 0,
+    y: 0,
+    rotationDeg: 0
+  });
+  var animLayer = new Kinetic.Layer();
+  var animGroup = new Kinetic.Group({
+    x: 0,
+    y: 0,
+    rotationDeg: 0
+  });
+  /*drow*/
+  if(!mountid){ mountid = 1;};
+  for (var i=0; i<mounts.length; i++) {
+    if (mounts[i].mid == mountid ){
+      /*drow point*/
+      var pointArr = mounts[i].points;
+      for (var n=0; n<pointArr.length; n++) {
+        //console.log(pointArr[n].ox + ","+ pointArr[n].oy);
+        (function() {
+          var k = n;
+          var newPoint = new Kinetic.Circle({
+            x: pointArr[k].ox/scale,
+            y: pointArr[k].oy/scale,
+            radius: 2,
+            fillRGB: {r:230,g:230,b:230},
+            shadowColorRGB: {r:255,g:255,b:255},
+            shadowBlur: 4,
+            strokeWidth: 0
+          });
+          if(mounts[i].type=="off"){
+            staticGroup.add(newPoint);
+          }else{
+            animGroup.add(newPoint);
+          }
+        })();
+      }
+      /*drow line*/
+      var linesArr =  mounts[i].lines;
+      for (var n=0; n<linesArr.length; n++) {
+        var strPoint = linesArr[n].strpid-1
+        , endPoint = linesArr[n].endpid-1
+        , PointX1 = pointArr[strPoint].ox/scale
+        , PointY1 = pointArr[strPoint].oy/scale
+        , PointX2 = pointArr[endPoint].ox/scale
+        , PointY2 = pointArr[endPoint].oy/scale;
+        var line = new Kinetic.Line({
+          x: 0,
+          y: 0,
+          id: "line"+n,
+          points: [PointX1,PointY1,PointX2,PointY2],
+          stroke: 'white',
+          opacity:0.4
+        });
+        console.log("lines-" + n +"-type:" + linesArr[n].type);
+          if(linesArr[n].type=="off"){
+            staticGroup.add(line);
+          }else{
+            animGroup.add(line);
+          }
+      }
+    }
+    //console.log(mountid)
+  }
+
+  /*drow animtion*/
+
+  var animLayer = new Kinetic.Layer();
+  var anims = new Object();
+  anims.a = 600;
+  anims.b = 1;
+  var anim = new Kinetic.Animation(function(frame) {
+    /*remove animlayer*/
+    animLayer.remove();
+    animLayer = new Kinetic.Layer();
+
+    console.log("frame:" + frame.timeDiff + ",frame.time :" + frame.time + ",anims.b:" + anims.b ) ;
+
+    if ( frame.time > anims.a ) {
+      anim.stop();
+      showSubFrame('takebox','take');
+      showNavBar('takepic');
+    };
+    anims.b++;
+    stage.add(animLayer);
+    //date1=new Date();
+  }, animLayer);
+  //console.log("staticGroup:" + staticGroup+"\n");
+  staticLayer.add(staticGroup);
+  stage.add(staticLayer);
+  animLayer.add(animGroup);
+  stage.add(animLayer);
+  
+
+  document.getElementById('start').addEventListener('click', function() {
+    anim.start();
+  }, false);
+  
+  document.getElementById('stop').addEventListener('click', function() {
+    anim.stop();
+  }, false);
+
+} //drowMout finish;
+
+
+
 var AppRouter = Backbone.Router.extend({  
     routes : {  
         '' : 'main', 
@@ -40,6 +197,7 @@ var AppRouter = Backbone.Router.extend({
     	if(!user){ user = 'male'}
         console.log('takePic 性别为：' +user);
     	showSubFrame('takebox','take');
+      showNavBar('takepic');
     },  
     retakePic : function(id) {  
         console.log('渲染详情方法, id为: ' + id);  
@@ -62,152 +220,48 @@ var AppRouter = Backbone.Router.extend({
 });  
   
 
-function showFrame(framename) {
-	if(!framename){ framename = 'homepage'}
-	$('.frame').hide();
-	if(framename !=='homepage' ){ $('.link_home').show();} else {$('.link_home').hide();};
-    $('.' + framename ).show();
-}
-
-function showSubFrame(framename,subframename) {
-	if(!framename && !subframename) {return false;};
-	showFrame(framename);
-	$('.' + framename + ' .subframe').hide();
-	$('.' + framename + ' .' + subframename).show();
-}
-
-function gotPic(event) {
-    if(event.target.files.length == 1 && 
-       event.target.files[0].type.indexOf("image/") == 0) {
-        $("#yourimage").attr("src",URL.createObjectURL(event.target.files[0]));
-    }
-}
-
-function drowMout(mountid) {
-  var scale = 2
-	, stage = new Kinetic.Stage({
-		container: 'container',
-		width: 300,
-		height: 280
-	});
-
-
-  var stage = new Kinetic.Stage({
-    container: 'container',
-    width: 300,
-    height: 280
-  });
-  var staticLayer = new Kinetic.Layer();
-  var staticGroup = new Kinetic.Group({
-    x: 0,
-    y: 0,
-    rotationDeg: 0
-  });
-  var animLayer = new Kinetic.Layer();
-  var animGroup = new Kinetic.Group({
-    x: 0,
-    y: 0,
-    rotationDeg: 0
-  });
-  /*drow*/
-  if(!mountid){ mountid = 1;};
-  for (var i=0; i<mounts.length; i++) {
-    if (mounts[i].mid == mountid ){
-      /*drow point*/
-      var pointArr = mounts[i].points;
-      for (var n=0; n<pointArr.length; n++) {
-        //console.log(pointArr[n].ox + ","+ pointArr[n].oy);
-        (function() {
-          var k = n;
-          var newPoint = new Kinetic.Circle({
-            x: pointArr[k].ox/scale,
-            y: pointArr[k].oy/scale,
-            radius: 2,
-            fillRGB: {r:230,g:230,b:230},
-            shadowColorRGB: {r:255,g:255,b:255},
-            shadowBlur: 4,
-            strokeWidth: 0
-          });
-          if(mounts[i].type=="on"){
-            staticGroup.add(newPoint);
-          }else{
-            animGroup.add(newPoint);
-          }
-        })();
-      }
-      /*drow line*/
-      var linesArr =  mounts[i].lines;
-      for (var n=0; n<linesArr.length; n++) {
-        var strPoint = linesArr[n].strpid-1
-        , endPoint = linesArr[n].endpid-1
-        , PointX1 = pointArr[strPoint].ox/scale
-        , PointY1 = pointArr[strPoint].oy/scale
-        , PointX2 = pointArr[endPoint].ox/scale
-        , PointY2 = pointArr[endPoint].oy/scale;
-        var line = new Kinetic.Line({
-          x: 0,
-          y: 0,
-          points: [PointX1,PointY1,PointX2,PointY2],
-          stroke: 'white',
-          opacity:0.4
-        });
-          if(linesArr[n].type=="on"){
-            staticGroup.add(line);
-          }else{
-            animGroup.add(line);
-          }
-      }
-    }
-    //console.log(mountid)
-  }
-
-  /*drow animtion*/
-
-  var animLayer = new Kinetic.Layer();
-  var anim = new Kinetic.Animation(function(frame) {
-    /*remove animlayer*/
-    animLayer.remove();
-    animLayer = new Kinetic.Layer();
-
-    console.log("frame:" + frame.timeDiff + ",frame.time :" + frame.time) ;
-
-    if ( frame.time > 600 ) {anim.stop();};
-    stage.add(animLayer);
-    //date1=new Date();
-  })
-  //console.log("staticGroup:" + staticGroup+"\n");
-  staticLayer.add(staticGroup);
-  stage.add(staticLayer);
-  animLayer.add(animGroup);
-  stage.add(animLayer);
-  
-
-  document.getElementById('start').addEventListener('click', function() {
-    anim.start();
-  }, false);
-  
-  document.getElementById('stop').addEventListener('click', function() {
-    anim.stop();
-  }, false);
-
-}
-
-
 var router = new AppRouter();  
 Backbone.history.start(); 
 
-
-
-
 $(document).ready(function() {
   console.log('onReady');
+
+  $('.takethepic').die('click').live('click',function(){
+    $('#takePictureField').click();
+
+    //$('#input1').val($('#myfile').val());
+  });
+
 	$("#takePictureField").on("change",gotPic);
+  $(".retake").die('click').live('click',function(){
+    postThePic();
+  });
+
 	//$("#yourimage").load(getSwatches);
 	desiredWidth = window.innerWidth;
-    
-    if(!("url" in window) && ("webkitURL" in window)) {
-        window.URL = window.webkitURL;   
-    }
+  if(!("url" in window) && ("webkitURL" in window)) {
+      window.URL = window.webkitURL;   
+  };
+  $('.mountswich .pre').die('click').live('click',function(){
+    showSubFrame('yourmount','rendering');
+    $('.mountswich a').hide();
+    //if 
+    $('.mountswich .nex').show();
+  });
+  $('.mountswich .nex').die('click').live('click',function(){
+    showSubFrame('yourmount','real');
+    showNavBar('replay');
+    $('.mountswich a').hide();
+    $('.mountswich .pre').show();
+  });
+  $('.navbox .youreal').die('click').live('click',function(){
+    showSubFrame('yourmount','real');
+    showNavBar('replay');
+    $('.mountswich a').hide();
+    $('.mountswich .pre').show();
+
+  });
 });
+
 
 
